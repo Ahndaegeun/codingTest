342f641fd0c40ccdcadb64123621c04dd2944a43
ahndaegeun <adg0807@naver.com>
2022-01-19 12:29:30 +0900

    신규아이디추천

--- src/main/신규아이디추천.java
+++ src/main/신규아이디추천.java
@@ -2,11 +2,33 @@ package main;
 
 public class 신규아이디추천 {
 
-    public static void solution(String new_id) {
+    public static String solution(String new_id) {
+        String answer = new_id;
+        answer = new_id.toLowerCase();
+        answer = answer.replaceAll("[^a-z0-9-._]", "");
+        answer = answer.replaceAll("[.]{2,}", ".");
+        answer = answer.replaceAll("^[.]", "");
+        answer = answer.replaceAll("[.]$", "");
+        if(answer.length() == 0) {
+            answer = "a";
+        }
+        if(answer.length() > 15) {
+            answer = answer.substring(0, 15);
+            answer = answer.replaceAll("[.]$", "");
+        }
+        if(answer.length() < 3) {
+            String lastChar = answer.substring(answer.length() - 1, answer.length());
+            for(int i = answer.length(); i < 3; i++) {
+                answer = answer + lastChar;
+            }
+        }
 
+        return answer;
     }
 
     public static void main(String[] args) {
-
+        solution("...!@BaT#*..y.abcdefghijklm"); // bat.y.abcdefghi
+        solution("z-+.^."); // z--
+        solution("=.="); // aaa
     }
 }
