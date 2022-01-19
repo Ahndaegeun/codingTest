b9d56f749e0d4cb7a659b70222af2126d10f1136
ahndaegeun <adg0807@naver.com>
2022-01-19 18:24:15 +0900

    크레인인형뽑기게임

--- src/main/level_1/크레인인형뽑기게임.java
+++ src/main/level_1/크레인인형뽑기게임.java
@@ -1,11 +1,29 @@
 package main.level_1;
 
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
+
 public class 크레인인형뽑기게임 {
 
     public static int solution(int[][] board, int[] moves) {
+        List<Integer> list = new ArrayList<>();
         int answer = 0;
 
-
+        for(int move : moves) {
+            for(int i = 0; i < board.length; i++) {
+                if(board[i][move - 1] != 0) {
+                    if(!list.isEmpty() && list.get(list.size() - 1) == board[i][move - 1]) {
+                        answer += 2;
+                        list.remove(list.size() - 1);
+                    } else {
+                        list.add(board[i][move - 1]);
+                    }
+                    board[i][move - 1] = 0;
+                    break;
+                }
+            }
+        }
         return answer;
     }
 
